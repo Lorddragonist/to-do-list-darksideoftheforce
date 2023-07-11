c8228a0a08fe896b4132ffd92f9a84bf3d1d6fcf
Andres Gonzalez <andrewgonzlez@gmail.com>
2023-07-11 09:08:46 -0500

    Clase 04

--- src/App.js
+++ src/App.js
@@ -5,22 +5,37 @@ import { TodoList } from './TodoList';
 import { TodoItem } from './TodoItem';
 import { CreateTodoButton } from './CreateTodoButton';
 import './App.css';
+import React from 'react';
+
+const defaultTodos = [
+  { text: 'Cortar cebolla', completed: false },
+  { text: 'Tomar el curso de Plazi', completed: false },
+  { text: 'Llorar con la llorona', completed: false },
+  { text: 'LALALALALALA', completed: false },
+
+];
 
 function App() {
   return (
-    <div className='App'>
-      <TodoCounter />
+    <React.Fragment>
+      <TodoCounter completed={16} total={25} />
+
       <TodoSearch />
 
       <TodoList>
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
+        {defaultTodos.map(todo => (
+          <TodoItem
+            key={todo.text} 
+            text={todo.text}
+            completed={todo.completed}
+          />
+        ))}
+
       </TodoList>
 
       <CreateTodoButton />
 
-    </div>
+    </React.Fragment>
   );
 }
 
--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,11 +1,11 @@
-function TodoItem() {
-    return (
-      <li>
-        <span>V</span>
-        <p>Llorar a la llorería</p>
-        <span></span>
-      </li>
-    );
-  }
+function TodoItem(props) {
+  return (
+    <li>
+      <span>V</span>
+      <p> {props.text} </p>
+      <span>X</span>
+    </li>
+  );
+}
 
-  export { TodoItem };
+export { TodoItem };
