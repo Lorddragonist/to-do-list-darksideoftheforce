d5181321866a68e9040931b56ee9cd3b6fc2c877
Andres Gonzalez <andrewgonzlez@gmail.com>
2023-07-10 21:32:23 -0500

    segmentando los componentes

---
+++ src/TodoItem.js
@@ -0,0 +1,11 @@
+function TodoItem() {
+    return (
+      <li>
+        <span>V</span>
+        <p>Llorar a la llorería</p>
+        <span></span>
+      </li>
+    );
+  }
+
+  export { TodoItem };
