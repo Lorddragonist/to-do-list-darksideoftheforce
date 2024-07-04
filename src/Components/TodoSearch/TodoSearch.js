ba04de28f117a8de8e6dab3c3ab07a03c7a7e84e
Andres Gonzalez <andrewgonzlez@gmail.com>
2024-07-03 21:00:45 -0500

    Agregando estilo lado oscuro de la fuerza

---
+++ src/Components/TodoSearch/TodoSearch.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './TodoSearch.css';
+
+function TodoSearch() {
+  const [searchValue, setSearchValue] = React.useState('');
+
+  console.log('Los usuarios buscan ' + searchValue);
+
+  return (
+    <input
+      placeholder="Search a task..."
+      className="TodoSearch"
+      value={searchValue}
+      onChange={(event) => {
+        setSearchValue(event.target.value);
+
+      }}
+    />
+  )
+}
+
+export { TodoSearch };
